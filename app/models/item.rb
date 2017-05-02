7d7d52e52aefc83d876bd263a108a312eed9d6e5
chimame <rito.tamata@gmail.com>
2017-05-02 15:05:14 +0900

    Add spec of using mock

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
   after_create :send_thanks_email
 
+  def method1
+    method2
+  end
+
+  def method2
+    "test"
+  end
+
   def send_thanks_email
     ItemMailer.thanks_email(self).deliver_now
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  example 'send mail when created' do
-    expect do
-      create(:item)
-    end.to change { ActionMailer::Base.deliveries.size }.by(1)
+  describe '#method1' do
+    let(:item) { Item.new }
+    subject { item.method1 }
+    before{ allow(item).to receive(:method2).and_return("mock") }
+
+    example 'return "mock"' do
+      is_expected.to eq 'mock'
+    end
+  end
+
+  describe 'before create' do
+    example 'send mail when created' do
+      expect do
+        create(:item)
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
   end
 end
